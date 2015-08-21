811303b9942971c0f1ee54cffe876c3ab570f360
grandtiger <grandtiger@gmail.com>
2015-08-21 12:39:03 -0400

    Add Prism 5 View-Switching Navigation_Desktop example

---
+++ Samples/Quickstarts/View-Switching Navigation_Desktop/ViewSwitchingNavigation.Email/EmailModule.cs
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+
+using System.ComponentModel.Composition;
+using Prism.Mef.Modularity;
+using Prism.Modularity;
+using Prism.Regions;
+using ViewSwitchingNavigation.Email.Views;
+using ViewSwitchingNavigation.Infrastructure;
+
+namespace ViewSwitchingNavigation.Email
+{
+    [ModuleExport(typeof(EmailModule))]
+    public class EmailModule : IModule
+    {
+        [Import]
+        public IRegionManager regionManager;
+
+        public void Initialize()
+        {
+            this.regionManager.RegisterViewWithRegion(RegionNames.MainNavigationRegion, typeof(EmailNavigationItemView));
+        }
+    }
+}
